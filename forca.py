9a4dc7dfb75083b9f0ba5fcd2bde3707aafe3e4b
Marcus Marçal <marcus.marcal@gmail.com>
2020-08-19 15:18:54 -0300

    Remoção de palavras com acento e outros sinais gráficos em palavras.txt. Organização do código em funções.

--- forca.py
+++ forca.py
@@ -1,45 +1,37 @@
 import random
+#import os #se necessário para função cla, lipar tela...
 
 def jogar():
-    print("*******************************************")
-    print("** Bem-vindo ao jogo de Forca do Dionei! **")
-    print("*******************************************")
+    mensagem_inicio() #Chama função da mensagem inicial do jogo
 
-    arquivo = open("palavras.txt", "r")
-    palavras = []
-    for linha in arquivo:
-        linha = linha.strip()
-        palavras.append(linha)
+    palavra_secreta = carrega_palavra() #Chama função que carrega a palavra secreta
 
-    tamanho = len(palavras)
-    escolhida = palavras[random.randrange(0, tamanho)]
+    letras_acertadas = carrega_letras_acertadas(palavra_secreta) #Carrega função de letras acertadas
 
-    arquivo.close()
-
-    palavra_secreta = escolhida.upper()
-    letras_acertadas = ["_" for letra in palavra_secreta]
+    letras_erradas = []
 
     enforcou = False
     acertou = False
-    erros = 0
+    erros = 10
 
-    print("Palavra: ", letras_acertadas)
+    print("Palavra incompleta: ", letras_acertadas)
 
     while (not acertou and not enforcou):
         chute = input("Qual letra? ")
         chute = chute.strip().upper()
 
-        if(chute in palavra_secreta):
+        if (chute in palavra_secreta):
             index = 0
             for letra in palavra_secreta:
-                if(chute == letra):
+                if (chute == letra):
                     letras_acertadas[index] = letra
-                    #print("Encontrou a letra {} na posição {}".format(chute, index))
+                    print("Encontrou a letra {} na posição {}".format(chute, index))
                 index += 1
         else:
-            erros += 1
-            print("Erros: {}".format(erros))
-            if(erros == 6):
+            erros -= 1
+            letras_erradas.append(chute)
+
+            if (erros == 0):
                 enforcou = True
                 print("****************** ENFORCOU!! ******************")
                 print("***** A PALAVRA ERA >>> {} <<< *****".format(palavra_secreta))
@@ -50,9 +42,12 @@ def jogar():
                 else:
                     print("************* ATÉ A PRÓXIMA! **************")
                     break
-        print("Palavra: ", letras_acertadas)
+        #print('\n' * 100)
+        #os.system('cls')
+        print("Letras erradas: {}".format(letras_erradas))
+        print("Erros disponíveis: {}".format(erros))
+        print("PALAVRA SECRETA: ", letras_acertadas)
         # print("Jogando...")
-
 
         if "_" not in letras_acertadas:
             acertou = True
@@ -66,5 +61,26 @@ def jogar():
                 print("************* ATÉ A PRÓXIMA! **************")
                 break
 
-if(__name__ == "__main__"):
+
+def mensagem_inicio():
+    print("*******************************************")
+    print("** Bem-vindo ao jogo de Forca do Dionei! **")
+    print("*******************************************")
+
+def carrega_palavra():
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+    tamanho = len(palavras)
+    escolhida = palavras[random.randrange(0, tamanho)].upper()
+    arquivo.close()
+    #palavra_secreta = unidecode(escolhida.upper())
+    return escolhida
+
+def carrega_letras_acertadas(palavra):
+    return ["_" for letra in palavra]
+
+if (__name__ == "__main__"):
     jogar()
